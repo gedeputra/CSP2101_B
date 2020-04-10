5a49254c7993d847fc2a1294e66828d8df155293
gede putra <gedeputra@github.com>
2020-04-10 06:33:16 +0000

    This is my portfolio1 file

--- portfolio1/guessage.sh
+++ portfolio1/guessage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #generating variable for age
-#generating variable for the answer random number (0-80)
+#generating variable for the answer random number (1-90)
 age=0
-answer=$(($RANDOM%90))
+answer=$(( ( $RANDOM % 90 )  + 1 ))
 #entering the loop for input console
 until [ $age -eq $answer ]; do
 #prompt the user to put the age until the answer is correct (input console)
@@ -14,7 +14,7 @@ elif [ $age -lt $answer ];then
 echo "Too low! Try again"
 fi
 done
-#print the final message after completing the loop by giving a correct answer
+#print the final message after completing the loop and print the correct answer as an output
 #exit after the game finished
-echo "You are correct! The answer is $answer congratulation!"
+echo "You are correct! The answer is $answer congratulations!"
 exit 0
